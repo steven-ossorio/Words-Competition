8dde9ee087c598df1cb023b3e78bf751e7e52c12
Steven <codebusiness2014@gmail.com>
2018-08-06 11:16:54 -0400

    Added webpack to the application

--- words/src/App.js
+++ words/src/App.js
@@ -1,16 +1,8 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.time = 60;
-    this.countDown = this.countDown.bind(this);
-  }
-
-  countDown() {}
   render() {
     return (
       <div className="App">
---
+++ words/webpack.config.js
@@ -0,0 +1,31 @@
+let path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  entry: "./index.js",
+  module: {
+      rules: [
+          {
+              test: [/\.jsx?$/, /\.js?$/],
+              exclude: /(node_modules|bower_components)/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['react', 'env']
+                }
+              }
+          }, {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      }, {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+    }
+      ]
+  },
+  output: {
+      path: __dirname + "/public/",
+      filename: "build.js"
+  }
+};
