aaf5ce4818d131ef3e0497a146732c448195111a
Steven <codebusiness2014@gmail.com>
2018-08-07 14:20:31 -0400

    Added node-sass package

--- words/webpack.config.js
+++ words/webpack.config.js
@@ -1,32 +1,37 @@
-let path = require('path');
+let path = require("path");
 
 module.exports = {
   context: path.join(__dirname, "src"),
   entry: "./index.js",
   module: {
-      rules: [
-          {
-              test: [/\.jsx?$/, /\.js?$/],
-              exclude: /(node_modules|bower_components)/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['react', 'env']
-                }
-              }
-          }, {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader'],
-      }, {
+    rules: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["react", "env"]
+          }
+        }
+      },
+      {
+        test: [/\.scss$/, /\.css$/],
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+      {
         test: /\.svg$/,
-        loader: 'svg-inline-loader'
-    }
-      ],
+        loader: "svg-inline-loader"
+      }
+    ]
   },
-  resolve: { extensions: ['.js', '.jsx'] },
+  resolve: { extensions: [".js", ".jsx"] },
   output: {
-      path: __dirname + "/src/javascripts",
-      filename: "build.js"
+    path: __dirname + "/src/javascripts",
+    filename: "build.js"
   }
 };
