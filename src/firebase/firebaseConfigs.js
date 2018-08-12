9267d4cc56ccf8f005fb2fa5fb1991e1187a6faf
Steven <codebusiness2014@gmail.com>
2018-08-11 20:46:19 -0400

    Removed unnecessary additional folder

---
+++ src/firebase/firebaseConfigs.js
@@ -0,0 +1,10 @@
+let config = {
+  apiKey: "AIzaSyBT6XoxqMkwP4hJQ0tC_E5ESuW9zKtCybM",
+  authDomain: "word-9b174.firebaseapp.com",
+  databaseURL: "https://word-9b174.firebaseio.com",
+  projectId: "word-9b174",
+  storageBucket: "",
+  messagingSenderId: "1095596524420"
+};
+
+export default config;
