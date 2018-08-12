3c9124be5f13fbfa5095ab7a439af82235a530d7
Steven <codebusiness2014@gmail.com>
2018-08-11 20:41:36 -0400

    Put obj back to how it was originally

--- words/src/firebase/secretKeys.js
+++ words/src/firebase/secretKeys.js
@@ -1,26 +1,14 @@
 import * as firebase from "firebase";
 import Config from "./firebaseConfigs.js";
 
-let config;
-if (process.env.NODE_ENV === "production") {
-  config = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId
-  };
-} else {
-  config = {
-    apiKey: Config.apiKey,
-    authDomain: Config.authDomain,
-    databaseURL: Config.databaseURL,
-    projectId: Config.projectId,
-    storageBucket: Config.storageBucket,
-    messagingSenderId: Config.messagingSenderId
-  };
-}
+let config = {
+  apiKey: Config.apiKey,
+  authDomain: Config.authDomain,
+  databaseURL: Config.databaseURL,
+  projectId: Config.projectId,
+  storageBucket: Config.storageBucket,
+  messagingSenderId: Config.messagingSenderId
+};
 
 export default (!firebase.apps.length
   ? firebase.initializeApp(config)
