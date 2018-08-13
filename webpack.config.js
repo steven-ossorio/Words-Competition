a01566c94f1cf5d3c8a58864b33753ba320f15b2
Steven <codebusiness2014@gmail.com>
2018-08-12 20:51:35 -0400

    Config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -71,7 +71,9 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
     })
   ]
 };
