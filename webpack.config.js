9c453ed0d85be91fd26efef07b90955c76046a09
Steven <codebusiness2014@gmail.com>
2018-08-12 20:44:22 -0400

    Changes to config plugin

--- webpack.config.js
+++ webpack.config.js
@@ -72,7 +72,6 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
-    }),
-    new webpack.optimize.UglifyJsPlugin()
+    })
   ]
 };
